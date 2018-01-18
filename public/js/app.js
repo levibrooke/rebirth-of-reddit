59d7c2c678989803cc8a511b2fdb86deebda7ff7
Levi Porter <levi@levibrooke.com>
2018-01-17 20:19:17 -1000

    Added event listener for nav links, clearing of feed cards

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,24 @@ function buildStructure() {
   let plusButton = document.createElement("button"); // create header button
   header.appendChild(plusButton);
   let nav = document.createElement("nav"); // create nav
+  nav.id = "board-nav";
   container.appendChild(nav);
   let navOne = document.createElement("a");
   navOne.innerText = "Board One";
+  navOne.id = "board-one";
   nav.appendChild(navOne);
   let navTwo = document.createElement("a");
   navTwo.innerText = "Board Two";
+  navTwo.id = "board-two";
   nav.appendChild(navTwo);
   let navThree = document.createElement("a");
   navThree.innerText = "Board Three";
+  navThree.id = "board-three";
   nav.appendChild(navThree);
+  let navFour = document.createElement("a");
+  navFour.innerText = "Board Four";
+  navFour.id = "board-four";
+  nav.appendChild(navFour);
   let main = document.createElement("main"); // create main
   container.appendChild(main);
   let ul = document.createElement("ul"); // create ul
@@ -30,7 +38,6 @@ function buildStructure() {
   container.appendChild(footer);
   body.appendChild(container);
 }
-buildStructure();
 
 // let header = document.createElement("header");
 // document.body.appendChild(header);
@@ -55,70 +62,101 @@ buildStructure();
 // let footer = document.createElement("footer");
 // document.body.appendChild(footer);
 
-window.onload = function() {
 
+function newRequest(subreddit) {
   // populate page w/ default feed
   let reqDefault = new XMLHttpRequest();
-  reqDefault.open("GET", "https://www.reddit.com/r/mls.json");
+  reqDefault.open("GET", "https://www.reddit.com/r/" + subreddit + ".json");
   reqDefault.send();
-  reqDefault.addEventListener("load", defaultFeed);
-
-  function defaultFeed() {
+  reqDefault.addEventListener("load", function() {
     let defaultResponse = JSON.parse(this.response);
+    buildCards(defaultResponse.data.children);
+  });
+}
+
+function buildCards(data) {
+  
+  data.forEach(function(element, index, array) {
+    // console.log(element);
+    let newArticle = document.createElement("li");
+    let imgContainer = document.createElement("div");
+    imgContainer.className = "img-container";
+    newArticle.appendChild(imgContainer);
+    let newImg = document.createElement("img");
     
-    defaultResponse.data.children.forEach(function(element, index, array) {
-      console.log(element);
-      let newArticle = document.createElement("li");
-      feed.appendChild(newArticle);
+    if (element.data.preview) { // if image provided set newImg
+      newImg.setAttribute("src", element.data.preview.images[0].source.url);
+    } else { // if no image provided get fallback
+      newImg.setAttribute("src", "assets/placeholder.jpg");
+    }
+    imgContainer.appendChild(newImg);
 
-      let imgContainer = document.createElement("div");
-      imgContainer.className = "img-container";
-      newArticle.appendChild(imgContainer);
-      let newImg = document.createElement("img");
-      
-      if (element.data.preview) { // if image provided set newImg
-        newImg.setAttribute("src", element.data.preview.images[0].source.url);
-      } else { // if no image provided get fallback
-        newImg.setAttribute("src", "assets/placeholder.jpg");
-      }
-      imgContainer.appendChild(newImg);
+    let newTitle = document.createElement("a");
+    newTitle.className = "title";
+    newTitle.setAttribute("href", element.data.url);
+    newTitle.innerText = element.data.title;
+    newArticle.appendChild(newTitle);
 
-      let newTitle = document.createElement("a");
-      newTitle.className = "title";
-      newTitle.setAttribute("href", element.data.url);
-      newTitle.innerText = element.data.title;
-      newArticle.appendChild(newTitle);
+    let newMeta = document.createElement("p");
+    newMeta.className = "post-meta";
+    newArticle.appendChild(newMeta);
+    let newAuthor = document.createElement("span");
+    newAuthor.className = "author";
+    newAuthor.innerHTML = "by " + element.data.author;
+    newMeta.appendChild(newAuthor);
 
-      let newMeta = document.createElement("p");
-      newMeta.className = "post-meta";
-      newArticle.appendChild(newMeta);
-      let newAuthor = document.createElement("span");
-      newAuthor.className = "author";
-      newAuthor.innerHTML = "by " + element.data.author;
-      newMeta.appendChild(newAuthor);
+    let newTimestamp = document.createElement("span");
+    let formatTimestamp = moment.unix(element.data.created_utc);
+    formatTimestamp = moment(formatTimestamp).fromNow();
+    newTimestamp.className = "timestamp";
+    newTimestamp.innerText = formatTimestamp;
+    newMeta.appendChild(newTimestamp);
 
-      let newTimestamp = document.createElement("span");
-      let formatTimestamp = moment.unix(element.data.created_utc);
-      formatTimestamp = moment(formatTimestamp).fromNow();
-      newTimestamp.className = "timestamp";
-      newTimestamp.innerText = formatTimestamp;
-      newMeta.appendChild(newTimestamp);
+    let newUpvotes = document.createElement("span");
+    newUpvotes.className = "upvotes";
+    newUpvotes.innerText = element.data.ups + " upvotes";
+    newMeta.appendChild(newUpvotes);
+    let newDescription = document.createElement("p");
+    newDescription.className = "description";
 
-      let newUpvotes = document.createElement("span");
-      newUpvotes.className = "upvotes";
-      newUpvotes.innerText = element.data.ups + " upvotes";
-      newMeta.appendChild(newUpvotes);
-      let newDescription = document.createElement("p");
-      newDescription.className = "description";
+    if (element.data.selftext === "") {
+      newDescription.innerText = "Link shared from: " + element.data.domain;
+    } else {
+    newDescription.innerText = element.data.selftext;
+    }
+    newArticle.appendChild(newDescription);      
+    
+    // append to #feed
+    // feed.innerHTML = "";
+    feed.appendChild(newArticle);
+  });
+}
 
-      if (element.data.selftext === "") {
-        newDescription.innerText = "Link shared from: " + element.data.domain;
-      } else {
-      newDescription.innerText = element.data.selftext;
-      }
-      newArticle.appendChild(newDescription);          
+// event listeners
+function eventListeners() {
+  document.getElementById("board-nav").addEventListener("click", selectBoard);
 
-    });
+  function selectBoard() {
+    if (event.target.id === "board-one") {
+      console.log("click one");
+      feed.innerHTML = "";
+      newRequest("mls");
+    } else if (event.target.id === "board-two") {
+      console.log("click two");
+      feed.innerHTML = "";
+      newRequest("footballtactics");
+    } else if (event.target.id === "board-three") {
+      console.log("click three");
+      feed.innerHTML = "";
+      newRequest("worldfootball");
+    } else if (event.target.id === "board-four") {
+      console.log("click four");
+      feed.innerHTML = "";
+      newRequest("bundesliga");
+    }
   }
+}
 
-}();
+buildStructure();
+newRequest("mls");
+eventListeners();
