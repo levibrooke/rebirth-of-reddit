37af5e8314ba0c75c0691ddbfd474facfcbf78fe
Levi Porter <levi@levibrooke.com>
2018-01-17 18:21:16 -1000

    Moved creation of html structure inside function

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,59 @@
-// create elements
-let header = document.createElement("header");
-document.body.appendChild(header);
-let nav = document.createElement("nav");
-document.body.appendChild(nav);
-let main = document.createElement("main");
-document.body.appendChild(main);
-let ul = document.createElement("ul");
-ul.id = "feed";
-main.appendChild(ul);
-let footer = document.createElement("footer");
-document.body.appendChild(footer);
+// create structure
+function buildStructure() {
+  let body = document.body;
+  let container = document.createElement("div"); // create container
+  container.id = "container";
+  let header = document.createElement("header"); // create header
+  container.appendChild(header);
+  let logo = document.createElement("img"); // create header logo
+  logo.setAttribute("src", "assets/logo.svg");
+  header.appendChild(logo);
+  let plusButton = document.createElement("button"); // create header button
+  header.appendChild(plusButton);
+  let nav = document.createElement("nav"); // create nav
+  container.appendChild(nav);
+  let navOne = document.createElement("a");
+  navOne.innerText = "Board One";
+  nav.appendChild(navOne);
+  let navTwo = document.createElement("a");
+  navTwo.innerText = "Board Two";
+  nav.appendChild(navTwo);
+  let navThree = document.createElement("a");
+  navThree.innerText = "Board Three";
+  nav.appendChild(navThree);
+  let main = document.createElement("main"); // create main
+  container.appendChild(main);
+  let ul = document.createElement("ul"); // create ul
+  ul.id = "feed";
+  main.appendChild(ul);
+  let footer = document.createElement("footer"); // create footer
+  container.appendChild(footer);
+  body.appendChild(container);
+}
+buildStructure();
+
+// let header = document.createElement("header");
+// document.body.appendChild(header);
+// let logo = document.createElement("img");
+// logo.setAttribute("src", "assets/logo.svg");
+// header.appendChild(logo);
+// let plusButton = document.createElement("button");
+// header.appendChild(plusButton);
+// let nav = document.createElement("nav");
+// document.body.appendChild(nav);
+// let navOne = document.createElement("a");
+// navOne.innerText = "Board One";
+// let navTwo = document.createElement("a");
+// navTwo.innerText = "Board Two";
+// let navThree = document.createElement("a");
+// navThree.innerText = "Board Three";
+// let main = document.createElement("main");
+// document.body.appendChild(main);
+// let ul = document.createElement("ul");
+// ul.id = "feed";
+// main.appendChild(ul);
+// let footer = document.createElement("footer");
+// document.body.appendChild(footer);
 
 window.onload = function() {
 
